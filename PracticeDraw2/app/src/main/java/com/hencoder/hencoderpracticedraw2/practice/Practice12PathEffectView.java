8c76370d905dacfbb93153e8eb7bfb15dc0ae241
吴焕楠 <wuhuannan@meizu.com>
2020-11-20 09:25:10 +0800

    Paint效果类学习

--- PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
+++ PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
@@ -2,8 +2,15 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.SumPathEffect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -42,16 +49,19 @@ public class Practice12PathEffectView extends View {
         // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
 
         // 第一处：CornerPathEffect
+        paint.setPathEffect(new CornerPathEffect(20F));
         canvas.drawPath(path, paint);
 
         canvas.save();
         canvas.translate(500, 0);
         // 第二处：DiscretePathEffect
+        paint.setPathEffect(new DiscretePathEffect(20F, 5F));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 200);
+        paint.setPathEffect(new DashPathEffect(new float[]{20, 10, 5, 10}, 0));
         // 第三处：DashPathEffect
         canvas.drawPath(path, paint);
         canvas.restore();
@@ -59,18 +69,27 @@ public class Practice12PathEffectView extends View {
         canvas.save();
         canvas.translate(500, 200);
         // 第四处：PathDashPathEffect
+        Path dashPath = new Path();
+        dashPath.lineTo(20, -30);
+        dashPath.lineTo(40, 0);
+        dashPath.close();
+        paint.setPathEffect(new PathDashPathEffect(dashPath, 40, 0, PathDashPathEffect.Style.MORPH));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 400);
         // 第五处：SumPathEffect
+        PathEffect dashEffect = new DashPathEffect(new float[]{20, 10}, 0);
+        PathEffect discreteEffect = new DiscretePathEffect(20, 5);
+        paint.setPathEffect(new SumPathEffect(dashEffect, discreteEffect));
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 400);
         // 第六处：ComposePathEffect
+        paint.setPathEffect(new ComposePathEffect(dashEffect, discreteEffect));
         canvas.drawPath(path, paint);
         canvas.restore();
     }
--- PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice13ShadowLayerView.java
+++ PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice13ShadowLayerView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -22,14 +23,12 @@ public class Practice13ShadowLayerView extends View {
         super(context, attrs, defStyleAttr);
     }
 
-    {
-        // 使用 Paint.setShadowLayer() 设置阴影
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        // 使用 Paint.setShadowLayer() 设置阴影
+        paint.setShadowLayer(10, 0, 0, Color.GREEN);
         paint.setTextSize(120);
         canvas.drawText("Hello HenCoder", 50, 200, paint);
     }
