e2cd816a551057283d4e73d2e85189e8756145b0
吴焕楠 <wuhuannan@meizu.com>
2020-11-23 18:45:13 +0800

    Canvas 对绘制的辅助 clipXXX() 和 Matrix

---
+++ PracticeDraw4/app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -0,0 +1,69 @@
+package com.hencoder.hencoderpracticedraw4.practice;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.hencoderpracticedraw4.R;
+
+public class Practice12CameraRotateFixedView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+    Point point1 = new Point(200, 200);
+    Point point2 = new Point(600, 200);
+    Camera camera = new Camera();
+
+    public Practice12CameraRotateFixedView(Context context) {
+        super(context);
+    }
+
+    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float centerX = point1.x + bitmap.getWidth() / 2F;
+        float centerY = point1.y + bitmap.getHeight() / 2F;
+
+        canvas.save();
+        camera.save();
+        camera.rotateX(30);
+        canvas.translate(centerX, centerY);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-centerX, -centerY);
+        camera.restore();
+        canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        centerX = point2.x + bitmap.getWidth() / 2F;
+        centerY = point2.y + bitmap.getHeight() / 2F;
+
+        canvas.save();
+        camera.save();
+        camera.rotateY(30);
+        canvas.translate(centerX, centerY);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-centerX, -centerY);
+        camera.restore();
+        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
+    }
+}
